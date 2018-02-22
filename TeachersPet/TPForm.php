1a6f4fc51f3574a9efc2c1d7ad611c0edf91d584
Scott Maxa <smaxa20@my.whitworth.edu>
2018-02-21 20:12:44 -0800

    New Forms page with PHP

---
+++ TeachersPet/TPForm.php
@@ -0,0 +1,11 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $rating = $_POST["rating"];
+    $review = $_POST["review"];
+
+    echo $name;
+    echo $email;
+    echo $rating;
+    echo $review;
+?>
